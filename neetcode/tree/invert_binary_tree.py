961952b004f17db88a48d93168ea17ba23e6bb66
Collin Kleest <collinkleest@gmail.com>
2022-07-21 08:39:42 -0400

    comments

--- neetcode/tree/invert_binary_tree.py
+++ neetcode/tree/invert_binary_tree.py
@@ -5,9 +5,12 @@ from neetcode.tree.tree_node import TreeNode
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # base case
         if not root:
             return root
         
+        # recursively invert nodes
+        # bsf (breath search first)
         temp = root.left
         root.left = root.right
         root.right = temp
